2f0cbcb6d47fbd374c2adfb6e0294e3e20a96c9a
Jamie Wooltorton <james_wooltorton@hotmail.com>
2021-03-15 08:41:53 +0000

    Correctly generating address

--- create_wallet.py
+++ create_wallet.py
@@ -1,20 +1,22 @@
 from src.wallet import Wallet
 
-# Generate a new wallet and private key
-w = Wallet()
+# # Generate a new wallet
+# w1 = Wallet()
+# phrase = w1.get_menmonic()
+# w1.show_exponent_info()
 
-# Create some message to be signed and verified
-data = b"some data"
-signature = w.sign_data(data)
-if not w.verify_message(signature, data):
-    print('Signature is valid')
-print(w.show)
-print(w.get_private_key())
-print(w.get_public_key())
+# Generate a new wallet from a mnemonic
+phrase = "hamster diagram private dutch cause delay private meat slide toddler razor book happy fancy gospel tennis maple dilemma loan word shrug inflict delay length"
+# phrase = "aware report movie exile buyer drum poverty supreme gym oppose float elegant"
+w2 = Wallet(phrase)
+phrase = w2.get_menmonic()
+w2.show_exponent_info()
+print(f"\nmenmonic: {phrase}")
 
-w.random_secret_exponent()
+msg = b"some message data"
 
+signature = w2.sign_message(msg)
+if not w2.verify_message(signature, msg):
+    print('\nThe signature is valid')
 
-# recovery_seed = "peace amazing couch oven meat stay between raise risk sunny load kitten exile athlete cart"
-# w.bip39(recovery_seed)
-# print(w.show)
+# print(w2.get_public_key())
